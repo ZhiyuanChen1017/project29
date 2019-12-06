edefb3cfd366c0a254d36dd15fe2917147329b0d
Yuanjun Ling <yl4273@columbia.edu>
2019-12-06 22:05:15 +0000

    create /sightings and change in urls.py

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import squirrel
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    squirrel_list = list(squirrel.objects.all())
+    context = {'squirrel_list': squirrel_list}
+    return render(request, 'sightings/index.html', context)
