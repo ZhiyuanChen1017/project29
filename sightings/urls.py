19e6b93df3a394a0bf8675bd9f82f42ddca63e69
Yuanjun Ling <yl4273@columbia.edu>
2019-12-06 22:47:16 +0000

    revise the sightings/urls.py and add views.add

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns =[
     path('sightings/', views.index, name='index'),
     path('map/', views.map, name='map'),
-    path('sightings/<str:unique_squirrel_id>/', views.edit_squirrel, name='edit')
+    path('sightings/add/', views.add, name='add'),
+    path('sightings/<str:unique_squirrel_id>/', views.edit_squirrel, name='edit'),
 ]
