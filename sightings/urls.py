57b8ad36a442604205146883f3c061860a534a5e
Zhiyuan Chen <zc2481@columbia.edu>
2019-12-06 22:30:23 +0000

    add edit into the sightings/urls.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[
     path('sightings/', views.index, name='index'),
     path('map/', views.map, name='map'),
+    path('sightings/<str:unique_squirrel_id>/', views.edit_squirrel, name='edit')
 ]
