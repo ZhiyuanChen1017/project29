c35d0ba807f0d72d7d7059427bdde93696b84577
Zhiyuan Chen <zc2481@columbia.edu>
2019-12-06 22:27:37 +0000

    write the views.sightings/squirrelid

--- sightings/views.py
+++ sightings/views.py
@@ -11,3 +11,18 @@ def index(request):
     squirrel_list = list(squirrel.objects.all())
     context = {'squirrel_list': squirrel_list}
     return render(request, 'sightings/index.html', context)
+
+def edit_squirrel(request, unique_squirrel_id):
+    squirrel_obj = squirrel.objects.get(unique_squirrel_id=unique_squirrel_id)
+
+    if request.method == 'POST':
+        form = squirrelform(request.POST, instance=squirrel_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('/sightings/')
+    else:
+        form = squirrelform(instance=squirrel_obj)
+    context = {
+            'form': form
+            }
+    return render(request, 'sightings/edit.html', context)
