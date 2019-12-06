d8bf02746a4bf9667b154193af88026a7fffcd00
Yuanjun Ling <yl4273@columbia.edu>
2019-12-06 22:21:01 +0000

    finish the map in views, need map.html

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('sightings/', views.index, name='index'),
+    path('map/', views.map, name='map'),
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import squirrel
 from django.http import HttpResponse
 
+def map(request):
+    squirrel_list = list(squirrel.objects.all())[:100]
+    context = {'sightings': squirrel_list}
+    return render(request, 'sightings/map.html', context)
+
 def index(request):
     squirrel_list = list(squirrel.objects.all())
     context = {'squirrel_list': squirrel_list}
